a74d18459e0cd41db334f6dc48501c90cc88ca30
Kresimir Sutalo <kresimir095@gmail.com>
2019-03-11 14:22:46 +0100

    First commit

---
+++ polozi_ba/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace polozi_ba.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
