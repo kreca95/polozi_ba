803d29445e4bffec6c0bed2bc4aa68b56ec6abb7
Kresimir Sutalo <kresimir095@gmail.com>
2019-03-13 19:39:29 +0100

    popravci na identity frameworku (nepotpuni)

---
+++ polozi_ba/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using polozi_ba.Data;
+using polozi_ba.Data.Models;
+using polozi_ba.Models;
+
+[assembly: HostingStartup(typeof(polozi_ba.Areas.Identity.IdentityHostingStartup))]
+namespace polozi_ba.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public IConfiguration Configuration { get; }
+        public IdentityHostingStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<PoloziBaContext>(options =>
+                    options.UseSqlServer(
+                        Configuration.GetConnectionString("DefaultConnection")));
+
+                services.AddDefaultIdentity<Korisnik>()
+                    .AddEntityFrameworkStores<PoloziBaContext>();
+            });
+        }
+    }
+}
---
+++ polozi_ba/Models/Predmet/DodajPredmetModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace polozi_ba.Models.Predmet
+{
+    public class DodajPredmetModel
+    {
+        public string Naziv { get; set; }
+        public bool Provjera { get; set; }
+
+    }
+}
